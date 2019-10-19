319d106316f4f0cb4847922ac298b387a034042c
Hussein  Abdeljabbar Ismail Martínez <hussein.odeh@gmail.com>
2019-10-19 17:51:02 +0200

    Update sshLog.sh

--- sshLog.sh
+++ sshLog.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
 #only auth key
+
+###CONSTANTS
 remoteIP=10.0.3.250
 remoteUSER=hassio
 remotePATH=/home/hassio/LOGS/volumio.log
-ssh $remoteUSER@$remoteIP 
+
+###MAIN
+ssh $remoteUSER@$remoteIP echo `date "+%d/%m/%Y %T"` $1
